0c06090a8e825b2859ce890d1f8995a412732153
julsimmy <jesimko@gmail.com>
2024-09-09 21:30:47 -0500

    adding author,price, etc.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Book.create!([
+  {title: "The life of a turtle", author: "unknown", price: 25, published_date: Date.new(2024, 10, 5)},
+  {title: "Outliers", author: "unknown", price: 25, published_date: Date.new(2024, 10, 5)},
+  {title: "The Lion King", author: "unknown", price: 25, published_date: Date.new(2024, 10, 5)},
+  {title: "Ready Player One", author: "unknown", price: 25, published_date: Date.new(2024, 10, 5)},
+  {title: "Ready Player Two", author: "unknown", price: 25, published_date: Date.new(2024, 10, 5)}
+])
